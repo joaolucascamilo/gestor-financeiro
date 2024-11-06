450d98caeb833b9b607c94f91fc13ee004c4d213
Ramos <joao.lucas.c.ramos@accenture.com>
2024-11-05 23:02:57 -0300

    Adicionando projeto - primeiro commit

---
+++ src/test/java/com/contacerta/gestor_financeiro/GestorFinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contacerta.gestor_financeiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorFinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
